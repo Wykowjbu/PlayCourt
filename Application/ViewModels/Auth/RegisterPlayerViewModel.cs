9a4314aa6a520f2d65fb802b6bab63407f191117
Wykowjbu <huynpde180519@fpt.edu.vn>
2026-02-02 21:01:49 +0700

    [FE][feature-views][regiser-page] Init Register Form For Business

--- Application/ViewModels/Auth/RegisterPlayerViewModel.cs
+++ Application/ViewModels/Auth/RegisterPlayerViewModel.cs
@@ -9,23 +9,28 @@ namespace Application.ViewModels.Auth
 {
     public  class RegisterPlayerViewModel
     {
-        [Required(ErrorMessage = "Vui lòng chọn vai trò.")]
+        [Required(ErrorMessage = "Please select a role.")]
         public string Role { get; set; } = "Player"; // Default value
 
-        [Required(ErrorMessage = "Vui lòng nhập họ tên.")]
+        [Required(ErrorMessage = "Please enter your full name.")]
         public string FullName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng nhập Email.")]
-        [EmailAddress(ErrorMessage = "Định dạng Email không hợp lệ.")]
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng nhập số điện thoại.")]
-        [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        [Phone(ErrorMessage = "Invalid phone number.")]
         public string PhoneNumber { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
-        [MinLength(8, ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên.")]
+        [Required(ErrorMessage = "Please enter your password.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+
+        [Required (ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
