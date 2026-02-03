c2965fe9c4075b6c559559d22f76387aa7753c4f
vohai04 <haivo3225@gmail.com>
2026-02-04 00:33:25 +0700

    fix data validation create new match ( ViewsModel)

--- Application/ViewModels/Matches/CreateMatchRequestViewModel.cs
+++ Application/ViewModels/Matches/CreateMatchRequestViewModel.cs
@@ -5,22 +5,22 @@ namespace Application.ViewModels.Matches
     public class CreateMatchRequestViewModel
     {
         [Required(ErrorMessage = "Please select a sport.")]
-        public string SelectedSport { get; set; } = "Pickleball";
+        public string SelectedSport { get; set; }
 
         [Required(ErrorMessage = "Please enter a venue.")]
-        public string Venue { get; set; } = "My Khe Pickleball Arena";
+        public string Venue { get; set; }
 
         [Required(ErrorMessage = "Please select date and time.")]
-        public string DateTime { get; set; } = "Friday, Oct 27 • 18:30 - 20:30";
+        public string DateTime { get; set; }
 
         [Range(1, 5, ErrorMessage = "Skill level must be between 1 and 5.")]
-        public int MinimumSkillLevel { get; set; } = 3;
+        public int MinimumSkillLevel { get; set; }
 
         [Range(1, 10, ErrorMessage = "Players needed must be between 1 and 10.")]
-        public int PlayersNeeded { get; set; } = 3;
+        public int PlayersNeeded { get; set; }
 
         [Required(ErrorMessage = "Please select a cost sharing mode.")]
-        public string CostSharingMode { get; set; } = "Split Equally";
+        public string CostSharingMode { get; set; }
 
         public List<string> AvailableSports { get; set; } = new();
         public List<string> AvailableVenues { get; set; } = new();
